9e1f8aa2f24c2fc098604af657dc3a9ac1ea2167
Gabriel Raby <grabyc@gmail.com>
2025-11-11 16:35:58 -0300

    first_agent

--- kaggle5days/main.py
+++ kaggle5days/main.py
@@ -1,5 +1,14 @@
+from dotenv import load_dotenv
+import os
+
+# load .env from project root
+load_dotenv()
+
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+
 def main():
     print("Hello from kaggle5days!")
+    print("key:", GOOGLE_API_KEY)
 
 
 if __name__ == "__main__":
